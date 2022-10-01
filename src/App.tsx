f955578864c9978ccd69952e1522174370e64b4f
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2022-10-01 00:51:26 -0400

    add to list ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Component, createSignal, createSelector, createEffect, For, Index } from "solid-js";
+import { Component, createSignal, createSelector, createEffect, For, Index, createMemo } from "solid-js";
 import { loop } from "./loop";
+
+type DataPoint = { value: number };
 
 const initialList = [{ value: 100 }, { value: 50 }, { value: 20 }];
 
-const Chart: Component<{ data: { value: number }[] }> = (props) => {
+const Chart: Component<{ data: DataPoint[] }> = (props) => {
   const DUR = 4000;
-  let prevList: { value: number }[] = [];
+  let prevList: DataPoint[] = [];
 
-  const [data, setData] = createSignal<{ value: number }[]>([]);
-  const [transitionList, setTransitionList] = createSignal<{ value: number }[]>(props.data);
+  const [data, setData] = createSignal<DataPoint[]>([]);
+  const [transitionList, setTransitionList] = createSignal<DataPoint[]>(props.data);
+
+  const isUpdating = () => prevList.some((o, i) => o.value !== data()[i].value);
 
   createEffect(() => {
+    if (isUpdating()) {
+      console.log({ prevList, dataProps: props.data });
+    }
     prevList = data();
     setData(props.data);
+    setTransitionList(props.data);
   });
 
   createEffect(() => {
-    console.log({ prevList, data: data() });
     data().forEach((d, idx, arr) => {
       if (prevList.length && prevList[idx] && prevList[idx].value !== arr[idx].value) {
+        console.log("loop! is updating!", { prevList, arr });
         loop({
           id: String(idx),
           initial: prevList[idx].value || 0,
@@ -28,6 +36,8 @@ const Chart: Component<{ data: { value: number }[] }> = (props) => {
         });
       }
     });
+
+    // console.log(data());
   });
 
   return (
@@ -40,20 +50,34 @@ const Chart: Component<{ data: { value: number }[] }> = (props) => {
         </For>
       </svg>
 
-      {/* <pre>{JSON.stringify(data(), null, 2)}</pre> */}
+      <pre>{JSON.stringify(data(), null, 2)}</pre>
       <pre>{JSON.stringify(transitionList(), null, 2)}</pre>
     </div>
   );
 };
 
 const App: Component = () => {
-  const [dataList, setDataList] = createSignal<{ value: number }[]>(initialList);
+  const [dataList, setDataList] = createSignal<DataPoint[]>(initialList);
 
   return (
     <div>
       <h1>Transition Value</h1>
       <div>
         <ul style={{ padding: 0 }}>
+          {/* <For each={dataList()}>
+            {(item, idx) => (
+              <li style={{ border: "1px dashed", "list-style": "none" }}>
+                <label>{item.value}</label>
+                <input
+                  type="number"
+                  value={item.value}
+                  onChange={(e) => {
+                    setDataList((list) => list.map((d, i) => (i === idx() ? { value: +e.currentTarget.value } : d)));
+                  }}
+                />
+              </li>
+            )}
+          </For> */}
           <Index each={dataList()}>
             {(item, idx) => (
               <li style={{ border: "1px dashed", "list-style": "none" }}>
@@ -75,7 +99,16 @@ const App: Component = () => {
         <Chart data={dataList()} />
       </div>
 
-      <div>{/* <button onClick={(e) => setDataList((list) => [...list, { value: Math.random() * 100 }])}>ADD</button> */}</div>
+      <div>
+        <button
+          onClick={(e) => {
+            setDataList((list) => [...list, { value: Math.random() * 100 }]);
+            // setDataList((list) => [...list, { value: 0 }]);
+          }}
+        >
+          ADD
+        </button>
+      </div>
     </div>
   );
 };
