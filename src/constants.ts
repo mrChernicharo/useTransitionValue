c257e056ef794c71972d0a91dfa82c2829242d73
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2022-10-01 11:21:06 -0400

    preparing package

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-export type DataPoint = { value: number };
-
-export const initialList = [{ value: 100 }, { value: 50 }, { value: 20 }];
--- src/useTransitionValue.ts
+++ src/useTransitionValue.ts
@@ -42,7 +42,7 @@ export const useTransitionValue = (config: {
     setInstances(id, "frameId", requestAnimationFrame(repeat));
   }
 
-  function repeat(timestamp: number) {
+  function repeat() {
     currTime = Date.now();
 
     setInstances(id, "isLooping", true);
@@ -58,39 +58,5 @@ export const useTransitionValue = (config: {
       cb(curr);
       setInstances(id, "isLooping", false);
     }
-    // console.log(unwrap(instances[id]));
-    // console.log({ curr, final });
   }
 };
-
-// LEGACY VERSION
-
-// export const wait = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
-// export const useTransitionValue = async (config: {
-//   start: number;
-//   final: number;
-//   duration: number;
-//   cb: (value: number) => void;
-// }) => {
-//   const { start, final, duration, cb } = config;
-
-//   let curr = start;
-//   let diff = final - start;
-//   let itCount = 60 * (duration / 1000);
-//   const step = diff / itCount;
-
-//   // console.time("transitionValue");
-//   // console.log({ itCount, diff, step });
-
-//   let i = 0;
-//   while (i <= itCount) {
-//     await wait(duration / itCount);
-
-//     i === itCount ? cb(final) : cb(curr);
-
-//     curr += step;
-//     i++;
-//   }
-//   // console.timeEnd("transitionValue");
-// };
